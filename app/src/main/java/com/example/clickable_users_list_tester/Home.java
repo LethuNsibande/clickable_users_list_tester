e334b0482bbea66888d5d00b753f302fc710c245
Lethukuthula Nsibande <lethunsibande16@gmail.com>
2023-07-17 20:43:30 +0200

    Initial commit - RecyclerView in order - not clickable.

---
+++ app/src/main/java/com/example/clickable_users_list_tester/Home.java
@@ -0,0 +1,81 @@
+package com.example.clickable_users_list_tester;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class Home extends Fragment {
+
+    private ArrayList<News> newsArrayList;
+    private String[] newsHeading;
+    private int[] imageResourceID;
+    private RecyclerView recyclerview;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dataInitialize();
+
+        recyclerview = view.findViewById(R.id.recyclerview);
+        recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerview.setHasFixedSize(true);
+
+        MyAdapter myAdapter = new MyAdapter(getContext(), newsArrayList);
+        recyclerview.setAdapter(myAdapter);
+        myAdapter.notifyDataSetChanged();
+    }
+
+    private void dataInitialize() {
+        newsArrayList = new ArrayList<>();
+
+        newsHeading = new String[]{
+                getString(R.string.head_1),
+                getString(R.string.head_2),
+                getString(R.string.head_3),
+                getString(R.string.head_4),
+                getString(R.string.head_5),
+                getString(R.string.head_6),
+                getString(R.string.head_7),
+                getString(R.string.head_8),
+                getString(R.string.head_9),
+                getString(R.string.head_10)
+        };
+
+        imageResourceID = new int[]{
+                R.drawable.a,
+                R.drawable.b,
+                R.drawable.c,
+                R.drawable.d,
+                R.drawable.e,
+                R.drawable.f,
+                R.drawable.g,
+                R.drawable.h,
+                R.drawable.i,
+                R.drawable.j
+        };
+
+        for (int i = 0; i < newsHeading.length; i++) {
+            News news = new News(newsHeading[1], imageResourceID[i]);
+            newsArrayList.add(news);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/clickable_users_list_tester/News.java
@@ -0,0 +1,11 @@
+package com.example.clickable_users_list_tester;
+
+public class News {
+    String heading;
+    int titleImage;
+
+    public News(String heading, int titleImage) {
+        this.heading = heading;
+        this.titleImage = titleImage;
+    }
+}
