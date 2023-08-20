e23455645feaf7d71e71b507868cc02b54a0c1c4
LethuNsibande <lethunsibande16@gmail.com>
2023-08-20 03:05:09 +0200

    Date Popup Added.

--- app/src/main/java/com/example/clickable_users_list_tester/CustomDialogActivity.java
+++ app/src/main/java/com/example/clickable_users_list_tester/CustomDialogActivity.java
@@ -1,6 +1,7 @@
 package com.example.clickable_users_list_tester;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -11,14 +12,21 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 
+import java.text.MessageFormat;
+
 public class CustomDialogActivity extends Activity {
     Dialog dialog;
-    AppCompatButton btnCustomDialog, btnDialogCancel, btnDialogSave;
+    AppCompatButton btnCustomDialog, btnDialogCancel, btnDialogAdd;
+    ImageView imgDateSelection;
+    EditText txtDialogCompletionDate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,15 +34,13 @@ public class CustomDialogActivity extends Activity {
         setContentView(R.layout.activity_custom_dialog);
 
         dialog = new Dialog(CustomDialogActivity.this);
-        dialog.setContentView(R.layout.custom_dialog_box);
+        dialog.setContentView(R.layout.layout_add_task_dialog);
         dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.custom_dialog_bg));
         dialog.setCancelable(false);
 
-        btnCustomDialog = findViewById(R.id.btnCustomDialog);
-        btnDialogSave = dialog.findViewById(R.id.btnDialogSave);
-        btnDialogCancel = dialog.findViewById(R.id.btnDialogCancel);
+        txtDialogCompletionDate = dialog.findViewById(R.id.txtDialogCompletionDate);
 
+        btnCustomDialog = findViewById(R.id.btnCustomDialog);
         btnCustomDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +48,15 @@ public class CustomDialogActivity extends Activity {
             }
         });
 
+        imgDateSelection = dialog.findViewById(R.id.imgDialogDateSelection);
+        imgDateSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDateDialog();
+            }
+        });
+
+        btnDialogCancel = dialog.findViewById(R.id.btnDialogCancel);
         btnDialogCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,12 +64,24 @@ public class CustomDialogActivity extends Activity {
             }
         });
 
-        btnDialogSave.setOnClickListener(new View.OnClickListener() {
+        btnDialogAdd = dialog.findViewById(R.id.btnDialogAdd);
+        btnDialogAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(CustomDialogActivity.this, "Save button clicked.", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
+                Toast.makeText(CustomDialogActivity.this, "Add button clicked.", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void openDateDialog() {
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                txtDialogCompletionDate.setText(MessageFormat.format("{0}/{1}/{2}", String.valueOf(year), String.valueOf(month + 1), day));
+            }
+        }, 2024, 01, 01);
+
+        dialog.show();
     }
 }
